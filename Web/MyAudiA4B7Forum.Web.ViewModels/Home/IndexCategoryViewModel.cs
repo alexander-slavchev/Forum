bff75885f768af46f733ac4e517474d5ad6bcb06
alexander-slavchev <a.slavchev19.48@gmail.com>
2020-12-23 18:32:35 +0200

    index view model and controller

---
+++ Web/MyAudiA4B7Forum.Web.ViewModels/Home/IndexCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyAudiA4B7Forum.Services.Mapping;
+using MyAudiA4Forum.Data.Models;
+
+namespace MyAudiA4B7Forum.Web.ViewModels.Home
+{
+    public class IndexCategoryViewModel : IMapFrom<Category>
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url => $"/{this.Name.Replace(' ', '-')}";
+    }
+}
