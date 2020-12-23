2cc9af4eb5e9bbbaa14593971c680c098f01bdfd
alexander-slavchev <a.slavchev19.48@gmail.com>
2020-12-23 18:30:34 +0200

    added IndexView

--- Data/MyAudiA4B7Forum.Data/Seeding/CategoriesSeeder.cs
+++ Data/MyAudiA4B7Forum.Data/Seeding/CategoriesSeeder.cs
@@ -1,12 +1,12 @@
-﻿using MyAudiA4Forum.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyAudiA4B7Forum.Data.Seeding
+﻿namespace MyAudiA4B7Forum.Data.Seeding
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using MyAudiA4Forum.Data.Models;
+
     public class CategoriesSeeder : ISeeder
     {
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
@@ -16,11 +16,20 @@ namespace MyAudiA4B7Forum.Data.Seeding
                 return;
             }
             var categories = new List<string> {"Weekly checks",
-                "Engine and Associated Systems",
+                "Petrol Engine",
+                "Diesel Engine",
                 "Automatic transmission",
                 "Manual Transmission",
                 "Brakes",
-                "Suspension"};
+                "Suspension",
+                "Wiring diagrams",
+                "Bodywork",
+                "Electronics",
+                "Quattro",
+                "Front Wheel Drive",
+                "S4 Dedicated",
+                "RS4 Dedicated",
+                "Miscellaneous",};
             foreach (var category in categories)
             {
                 await dbContext.Categories.AddAsync(new Category
--- Web/MyAudiA4B7Forum.Web/Controllers/HomeController.cs
+++ Web/MyAudiA4B7Forum.Web/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
-﻿namespace MyAudiA4B7Forum.Web.Controllers
+﻿using System.Diagnostics;
+
+using MyAudiA4B7Forum.Web.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+using MyAudiA4B7Forum.Web.ViewModels.Home;
+using System.Linq;
+using MyAudiA4B7Forum.Data.Common.Repositories;
+using MyAudiA4Forum.Data.Models;
+using MyAudiA4B7Forum.Services.Mapping;
+
+namespace MyAudiA4B7Forum.Web.Controllers
 {
-    using System.Diagnostics;
-
-    using MyAudiA4B7Forum.Web.ViewModels;
-
-    using Microsoft.AspNetCore.Mvc;
-
     public class HomeController : BaseController
     {
+        private readonly IDeletableEntityRepository<Category> categoriesRepository;
+        public HomeController(IDeletableEntityRepository<Category> categoriesRepository)
+        {
+            this.categoriesRepository = categoriesRepository;
+        }
         public IActionResult Index()
         {
-            return this.View();
+            var viewModel = new IndexViewModel();
+            var categories = this.categoriesRepository.All()
+                .To<IndexCategoryViewModel>()
+                .ToList();
+            viewModel.Categories = categories;
+            return this.View(viewModel);
         }
 
         public IActionResult Privacy()
